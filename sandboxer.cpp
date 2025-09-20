93a471195b0673417b4a45b62ec61b11eb65e800
Sandu Bogdan <bogdanelsandu@hotmail.com>
2025-09-20 12:52:13 +0300

    Did more stuff

--- sandboxer.cpp
+++ sandboxer.cpp
@@ -31,131 +31,138 @@ class CPU {
         }
 
     public:
+        void execute(std::byte* content, uint16_t size) {
+            v0 = 0; pc = 0; r0 = 0; r1 = 0; r2 = 0; r3 = 0; r4 = 0; r5 = 0;
 
-    void execute(std::byte* content, uint16_t size) {
-        v0 = 0; pc = 0; r0 = 0; r1 = 0; r2 = 0; r3 = 0; r4 = 0; r5 = 0;
+            while (true) {
+                // Fetch instruction
+                uint8_t instruction = byte_to_int_be(content[pc]);
+                pc++;
 
-        while (true) {
-            // Fetch instruction
-            uint8_t instruction = byte_to_int_be(content[pc]);
-            pc++;
-
-            switch (instruction) {
-                case (0): {
-                    // HALT
-                    return;
-                } case (1): {
-                    // NOP
-                    __asm__("nop");
-                    break;
-                } case (2): {
-                    // mov reg<-immediate
-                    uint16_t reg = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    uint16_t imm = two_bytes_to_int_be({content[pc], content[pc + 1]});
-                    pc += 2;
-                    reg = imm;
-                    break;
-                } case (3): {
-                    // mov reg<-reg
-                    uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    uint16_t &reg2 = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    reg = reg2;
-                    break;
-                } case (4): {
-                    // add destination, reg1, reg2
-                    uint16_t &destination = get_reg(byte_to_int_be(content[pc]));        pc++;
-                    uint16_t &reg1 = get_reg(byte_to_int_be(content[pc]));               pc++;
-                    uint16_t &reg2 = get_reg(byte_to_int_be(content[pc]));               pc++;
-                    destination = reg1 + reg2;
-                    break;
-                } case (5): {
-                    // add destination, reg1, immediate
-                    uint16_t &destination = get_reg(byte_to_int_be(content[pc]));        pc++;
-                    uint16_t &reg1 = get_reg(byte_to_int_be(content[pc]));               pc++;
-                    uint16_t imm = two_bytes_to_int_be({content[pc], content[pc + 1]});
-                    pc += 2;
-                    destination = reg1 + imm;
-                    break;
-                } case (6): {
-                    // str <reg> <address>
-                    uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    uint16_t address = two_bytes_to_int_be({content[pc], content[pc + 1]});
-                    pc += 2;
-                    reg = two_bytes_to_int_be({content[to_relative(address, size) - 1], content[to_relative(address, size)]});
-                    break;
-                } case (7): {
-                    // str <address> <reg>
-                    uint16_t address = two_bytes_to_int_be({content[pc], content[pc + 1]});
-                    pc += 2;
-                    uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    content[to_relative(address, size) - 1] = int_to_byte(reg) >> 8;
-                    content[to_relative(address, size)] = int_to_byte(reg & 0xFF);
-                    break;
-                } case (8): {
-                    // str <address> <immediate>
-                    uint16_t address = two_bytes_to_int_be({content[pc], content[pc + 1]});
-                    pc += 2;
-                    uint16_t immediate = two_bytes_to_int_be({content[pc], content[pc + 1]});
-                    pc += 2;
-                    content[address] = int_to_byte(immediate) >> 8;
-                    content[address+1] = int_to_byte(immediate & 0xFF);
-                    break;
-                } case (9): {
-                    // print
-                    std::cout << ascii_bytes_to_string(int_to_bytes((int16_t) v0));
-                    break;
-                } case (10): {
-                    // printliteral
-                    std::cout << v0;
-                    break;
-                } case (11): {
-                    // str &reg reg
-                    uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
-                    uint16_t destination = reg;
-                    uint16_t &reg2 = get_reg(byte_to_int_be(content[pc + 1]));
-                    pc += 2;
-                    content[to_relative(destination, size) - 1] = int_to_byte(reg2) >> 8;
-                    content[to_relative(destination, size)] = int_to_byte(reg2 & 0xFF);
-                    break;
-                } case (12): {
-                    // str &reg imm
-                    uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
-                    uint16_t destination = reg;
-                    uint16_t immediate = two_bytes_to_int_be({content[pc + 1], content[pc + 2]});
-                    pc += 3;
-                    content[to_relative(destination, size) - 1] = int_to_byte(immediate);
-                    break;
-                } case (13): {
-                    // str reg &reg
-                    uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    uint16_t &reg2 = get_reg(byte_to_int_be(content[pc]));
-                    pc++;
-                    uint16_t source = reg2;
-                    reg = two_bytes_to_int_be({content[to_relative(source, size) - 1], content[to_relative(source, size)]});
-                    break;
-                } default: {
-                    std::cerr << "Invalid instruction: " << instruction << std::endl;
-                    exit(1);
+                switch (instruction) {
+                    case (0): {
+                        // HALT
+                        return;
+                    } case (1): {
+                        // NOP
+                        __asm__("nop");
+                        break;
+                    } case (2): {
+                        // mov reg<-immediate
+                        uint16_t reg = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        uint16_t imm = two_bytes_to_int_be({content[pc], content[pc + 1]});
+                        pc += 2;
+                        reg = imm;
+                        break;
+                    } case (3): {
+                        // mov reg<-reg
+                        uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        uint16_t &reg2 = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        reg = reg2;
+                        break;
+                    } case (4): {
+                        // add destination, reg1, reg2
+                        uint16_t &destination = get_reg(byte_to_int_be(content[pc]));        pc++;
+                        uint16_t &reg1 = get_reg(byte_to_int_be(content[pc]));               pc++;
+                        uint16_t &reg2 = get_reg(byte_to_int_be(content[pc]));               pc++;
+                        destination = reg1 + reg2;
+                        break;
+                    } case (5): {
+                        // add destination, reg1, immediate
+                        uint16_t &destination = get_reg(byte_to_int_be(content[pc]));        pc++;
+                        uint16_t &reg1 = get_reg(byte_to_int_be(content[pc]));               pc++;
+                        uint16_t imm = two_bytes_to_int_be({content[pc], content[pc + 1]});
+                        pc += 2;
+                        destination = reg1 + imm;
+                        break;
+                    } case (6): {
+                        // str <reg> <address>
+                        uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        uint16_t address = two_bytes_to_int_be({content[pc], content[pc + 1]});
+                        pc += 2;
+                        reg = two_bytes_to_int_be({content[to_relative(address, size) - 1], content[to_relative(address, size)]});
+                        break;
+                    } case (7): {
+                        // str <address> <reg>
+                        uint16_t address = two_bytes_to_int_be({content[pc], content[pc + 1]});
+                        pc += 2;
+                        uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        content[to_relative(address, size) - 1] = int_to_byte(reg) >> 8;
+                        content[to_relative(address, size)] = int_to_byte(reg & 0xFF);
+                        break;
+                    } case (8): {
+                        // str <address> <immediate>
+                        uint16_t address = two_bytes_to_int_be({content[pc], content[pc + 1]});
+                        pc += 2;
+                        uint16_t immediate = two_bytes_to_int_be({content[pc], content[pc + 1]});
+                        pc += 2;
+                        content[address] = int_to_byte(immediate) >> 8;
+                        content[address+1] = int_to_byte(immediate & 0xFF);
+                        break;
+                    } case (9): {
+                        // print
+                        std::cout << ascii_bytes_to_string(int_to_bytes((int16_t) v0));
+                        break;
+                    } case (10): {
+                        // printliteral
+                        std::cout << v0;
+                        break;
+                    } case (11): {
+                        // str &reg reg
+                        uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
+                        uint16_t destination = reg;
+                        uint16_t &reg2 = get_reg(byte_to_int_be(content[pc + 1]));
+                        pc += 2;
+                        content[to_relative(destination, size) - 1] = int_to_byte(reg2) >> 8;
+                        content[to_relative(destination, size)] = int_to_byte(reg2 & 0xFF);
+                        break;
+                    } case (12): {
+                        // str &reg imm
+                        uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
+                        uint16_t destination = reg;
+                        uint16_t immediate = two_bytes_to_int_be({content[pc + 1], content[pc + 2]});
+                        pc += 3;
+                        content[to_relative(destination, size) - 1] = int_to_byte(immediate);
+                        break;
+                    } case (13): {
+                        // str reg &reg
+                        uint16_t &reg = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        uint16_t &reg2 = get_reg(byte_to_int_be(content[pc]));
+                        pc++;
+                        uint16_t source = reg2;
+                        reg = two_bytes_to_int_be({content[to_relative(source, size) - 1], content[to_relative(source, size)]});
+                        break;
+                    } default: {
+                        std::cerr << "Invalid instruction: " << instruction << std::endl;
+                        exit(1);
+                    }
                 }
             }
-        }
-    };
-
+        };
 };
 
-int main(int argc, char **argv) {
-    char* filename = argv[1];
+struct data {
+    std::byte* content;
+    uint16_t size;
 
+    data() : content(nullptr), size(0) {}
+
+    ~data() {
+        delete[] content;
+    }
+};
+
+data load_file(char* filename) {
     std::ifstream file(filename, std::ios::binary | std::ios::ate);
     if (!file) {
         std::cerr << "Failed to open file: " << filename << "\n";
-        return 1;
+        std::exit(1);
     }
 
     // get size
@@ -171,12 +178,26 @@ int main(int argc, char **argv) {
     if (!file.read(reinterpret_cast<char*>(buffer), size)) {
         std::cerr << "Failed to read file.\n";
         delete[] buffer;
-        return 1;
+        std::exit(1);
     }
 
-    CPU cpu;
-    cpu.execute(buffer, buffer_size);
+    data d = data();
 
-    delete[] buffer;
+    d.content = *content;
+    d.size = buffer_size;
+
+    return d;
+}
+
+int main(int argc, char **argv) {
+    char* filename = argv[1];
+
+    data d = load_file(filename);
+    std::byte* content = d.content;
+    uint8_t buffer_size = d.size;
+
+    CPU cpu;
+    cpu.execute(content, buffer_size);
+
     return 0;
 }

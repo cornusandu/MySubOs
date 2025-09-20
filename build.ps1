c66ffabe37ca8216094a35d9bb36c69bd0cec67d
Sandu Bogdan <bogdanelsandu@hotmail.com>
2025-09-20 13:14:30 +0300

    .

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
-g++ sandboxer.cpp -O3 -o main.exe -static-libgcc -static-libstdc++ -m32
+g++ sandboxer.cpp -O3 -o main.exe -static-libgcc -static-libstdc++ -m64
 pip install Cython
+pip install pybind11
+pip install setuptools wheel
 python -m cython writecode.py -o writecode.c
-gcc writecode.c -O3 -o writecode.exe -static-libgcc -static-libstdc++ -m32
+gcc writecode.c -O3 -o writecode.exe -municode -emainCRTStartup -static-libgcc -static-libstdc++ -IC:/Users/Bogdan/AppData/Local/Programs/Python/Python313/include -LC:/Users/Bogdan/AppData/Local/Programs/Python/Python313/libs -lpython313 -m64
